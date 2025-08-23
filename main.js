4498ddffa815b47430541fb06ccdd3caef13b0bb
Alexis <alesixvaldivia@gmail.com>
2025-08-23 19:29:44 -0300

    inicializando el proyecto

---
+++ main.js
@@ -0,0 +1,5 @@
+addEventListener('DOMContentLoaded', () => {
+    console.log('JS');
+    const $ = (elem) => document.getElementById(elem);
+    const $$ = (elem) => document.querySelector(elem);
+})
