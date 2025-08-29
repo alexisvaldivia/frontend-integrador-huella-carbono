151580cf0d96865f0bff3b15f7219cae6ae32b4f
Alexis <alesixvaldivia@gmail.com>
2025-08-28 22:24:01 -0300

    agregando comentarios en el codigo

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
+// Estas dos funciones siempre las defino cuando trabajo con JS Vanilla y tengo que hacer gastantes gets del html, me ayudan a no repetir tanto los metodos.
 const $ = (elem) => document.getElementById(elem);
 const $$ = (elem) => document.querySelector(elem);
 
 addEventListener('DOMContentLoaded', () => {
+    // A penas cargue todo el contenido del HTML, me defino las variables/constantes que voy a necesitar para trabajar en la recoleccion de datos.
     const tipoCombustible = {
         nafta: 'nafta',
         gnc: 'Gas Natural Comprimido',
@@ -23,8 +25,46 @@ addEventListener('DOMContentLoaded', () => {
         cantidadKmBicicleta: 0,
         cantidadKmAvion: 0
     }
-
+    const respuestasConsumo = {
+        gastoRopa: 0,
+        segundaMano: null,
+        recicla: null,
+        cantidadDispositivos: 0,
+        renovacionDispositivos: null
+    }
 
     const formularioVehiculo = $('formulario-vehiculo');
+
+    // Acceso al evento 'submit' del formulario, y agrego la logica para recolectar los datos y mostrarlos en la consola.
+    formularioVehiculo.addEventListener('submit', (e) => {
+        e.preventDefault()
+        respuestasConsumoVehiculo.cantidadKmAutoPorSemana = Number(formularioVehiculo.elements['cantidad-km-auto'].value);
+        respuestasConsumoVehiculo.tipoCombustible = formularioVehiculo.elements['tipo-de-combustible'].value;
+        respuestasConsumoVehiculo.cantidadKmColectivo = Number(formularioVehiculo.elements['cantidad-km-colectivo'].value);
+        respuestasConsumoVehiculo.cantidadKmBicicleta = Number(formularioVehiculo.elements['cantidad-km-bicleta'].value);
+
+        let avion = $$("input[name = 'avion']:checked").value;
+
+        switch (avion) {
+            case 'no-viajo':
+                respuestasConsumoVehiculo.cantidadKmAvion = viajesAvion.noViajo;
+                break;
+            case 'poco':
+                respuestasConsumoVehiculo.cantidadKmAvion = viajesAvion.de500a1000;
+                break;
+            case 'medio':
+                respuestasConsumoVehiculo.cantidadKmAvion = viajesAvion.de1000a2500;
+                break;
+            case 'alto':
+                respuestasConsumoVehiculo.cantidadKmAvion = viajesAvion.de2500a5000;
+                break;
+            default:
+                alert('Valor de vuelos no aceptado');
+        }
+
+        console.log(respuestasConsumoVehiculo);
+    }
+    )
+
 
 })
