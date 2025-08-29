c2f381d3a9810a9891bd2509918f2548b089e410
Alexis <alesixvaldivia@gmail.com>
2025-08-28 22:20:45 -0300

    html del formulario para la encuesta basada en el transporte y la logica de recoleccion de datos al hacer submit en el formulario terminado

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
+const $ = (elem) => document.getElementById(elem);
+const $$ = (elem) => document.querySelector(elem);
+
 addEventListener('DOMContentLoaded', () => {
-    console.log('JS');
-    const $ = (elem) => document.getElementById(elem);
-    const $$ = (elem) => document.querySelector(elem);
+    const tipoCombustible = {
+        nafta: 'nafta',
+        gnc: 'Gas Natural Comprimido',
+        diesel: 'Diesel',
+        electrico: 'Electrico'
+    }
+
+    const viajesAvion = {
+        noViajo: 0,
+        de500a1000: 1500 / 2,
+        de1000a2500: 3500 / 2,
+        de2500a5000: 7500 / 2
+    }
+
+    const respuestasConsumoVehiculo = {
+        cantidadKmAutoPorSemana: 0,
+        tipoCombustible: null,
+        cantidadKmColectivo: 0,
+        cantidadKmBicicleta: 0,
+        cantidadKmAvion: 0
+    }
+
+
+    const formularioVehiculo = $('formulario-vehiculo');
+
 })
